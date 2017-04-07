38819a8426ba0099177d7a0986fa71cf7b3a50bb
oittaa <eero@oittaa.net>
2017-04-08 00:19:05 +0300

    Added tests and refactored uuid4() a bit to get 100% code coverage

---
+++ tests/uuidTest.php
@@ -0,0 +1,71 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers UUID
+ */
+final class uuidTest extends TestCase
+{
+    public function testCanGenerateValidVersion3()
+    {
+        $this->assertEquals(
+            '11a38b9a-b3da-360f-9353-a5a725514269',
+            UUID::uuid3(UUID::NAMESPACE_DNS, 'php.net')
+        );
+    }
+
+    public function testCanGenerateValidVersion4()
+    {
+        $uuid1 = UUID::uuid4();
+        $this->assertRegExp(
+            '/^[0-9a-f]{8}\-[0-9a-f]{4}\-[0-9a-f]{4}\-[0-9a-f]{4}\-[0-9a-f]{12}$/',
+            $uuid1
+        );
+        $uuid2 = UUID::uuid4();
+        $this->assertNotEquals(
+            $uuid1,
+            $uuid2
+        );
+    }
+
+    public function testCanGenerateValidVersion5()
+    {
+        $this->assertEquals(
+            'c4a760a8-dbcf-5254-a0d9-6a4474bd1b62',
+            UUID::uuid5(UUID::NAMESPACE_DNS, 'php.net')
+        );
+    }
+
+    public function testCannotBeCreatedFromInvalidNamespace()
+    {
+        $this->expectException(InvalidArgumentException::class);
+
+        UUID::uuid5('invalid', 'php.net');
+    }
+
+    public function testCanValidate()
+    {
+        $this->assertTrue(
+            UUID::isValid('11a38b9a-b3da-360f-9353-a5a725514269')
+        );
+        $this->assertTrue(
+            UUID::isValid('urn:uuid:c4a760a8-dbcf-5254-a0d9-6a4474bd1b62')
+        );
+        $this->assertTrue(
+            UUID::isValid('{C4A760A8-DBCF-5254-A0D9-6A4474BD1B62}')
+        );
+        $this->assertTrue(
+            UUID::equals(
+                'urn:uuid:c4a760a8-dbcf-5254-a0d9-6a4474bd1b62',
+                '{C4A760A8-DBCF-5254-A0D9-6A4474BD1B62}'
+            )
+        );
+        $this->assertFalse(
+            UUID::equals(
+                'c4a760a8-dbcf-5254-a0d9-6a4474bd1b62',
+                '2140a926-4a47-465c-b622-4571ad9bb378'
+            )
+        );
+    }
+}
--- uuid.php
+++ uuid.php
@@ -102,11 +102,7 @@ class UUID
      * @return string
      */
     public static function uuid4() {
-        if (function_exists('random_bytes')) {
-            $bytes = random_bytes(16);
-        } else {
-            $bytes = openssl_random_pseudo_bytes(16);
-        }
+        $bytes = function_exists('random_bytes') ? random_bytes(16) : openssl_random_pseudo_bytes(16);
         $hash = bin2hex($bytes);
         return self::uuidFromHash($hash, 4);
     }
