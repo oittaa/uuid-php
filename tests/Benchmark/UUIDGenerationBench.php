efa8970b08799299653c99fa62d7c234823b513e
Oittaa <eero@oittaa.net>
2021-11-04 05:44:10 +0100

    revs and iterations in phpbench.json

--- tests/Benchmark/UUIDGenerationBench.php
+++ tests/Benchmark/UUIDGenerationBench.php
@@ -8,37 +8,21 @@ use UUID\UUID;
 
 class UUIDGenerationBench
 {
-    /**
-     * @Revs(1000)
-     * @Iterations(5)
-     */
     public function benchUUID3Generation(): void
     {
         UUID::uuid3(UUID::NAMESPACE_DNS, 'php.net');
     }
 
-    /**
-     * @Revs(1000)
-     * @Iterations(5)
-     */
     public function benchUUID4Generation(): void
     {
         UUID::uuid4();
     }
 
-    /**
-     * @Revs(1000)
-     * @Iterations(5)
-     */
     public function benchUUID5Generation(): void
     {
         UUID::uuid5(UUID::NAMESPACE_DNS, 'php.net');
     }
 
-    /**
-     * @Revs(1000)
-     * @Iterations(5)
-     */
     public function benchUUID6Generation(): void
     {
         UUID::uuid6();
