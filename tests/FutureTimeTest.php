1d76242610d7bb472c929727ba0ed6948348389b
oittaa <8972248+oittaa@users.noreply.github.com>
2021-12-17 05:21:07 +0100

    Type declarations and cleanup (#26)

--- tests/FutureTimeTest.php
+++ tests/FutureTimeTest.php
@@ -24,14 +24,22 @@ final class FutureTimeTest extends TestCase
         $property->setValue($a, 9999990);
     }
 
+    protected function tearDown(): void
+    {
+        $a = new UUID();
+        $reflection = new \ReflectionClass($a);
+        $property = $reflection->getProperty('unixts');
+        $property->setAccessible(true);
+        $property->setValue($a, 0);
+        $property = $reflection->getProperty('subsec');
+        $property->setAccessible(true);
+        $property->setValue($a, 0);
+    }
+
     public function testFutureTimeVersion6()
     {
         $uuid1 = UUID::uuid6();
         for ($x = 0; $x < 1000; $x++) {
-            $this->assertMatchesRegularExpression(
-                '/^[0-9a-f]{8}\-[0-9a-f]{4}\-6[0-9a-f]{3}\-[89ab][0-9a-f]{3}\-[0-9a-f]{12}$/',
-                $uuid1
-            );
             $uuid2 = UUID::uuid6();
             $this->assertGreaterThan(
                 $uuid1,
@@ -45,10 +53,6 @@ final class FutureTimeTest extends TestCase
     {
         $uuid1 = UUID::uuid7();
         for ($x = 0; $x < 1000; $x++) {
-            $this->assertMatchesRegularExpression(
-                '/^[0-9a-f]{8}\-[0-9a-f]{4}\-7[0-9a-f]{3}\-[89ab][0-9a-f]{3}\-[0-9a-f]{12}$/',
-                $uuid1
-            );
             $uuid2 = UUID::uuid7();
             $this->assertGreaterThan(
                 $uuid1,
