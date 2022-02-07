431812869f13b263fc84bb860c84683e5caf7738
oittaa <8972248+oittaa@users.noreply.github.com>
2022-02-07 04:54:19 +0100

    Less magic numbers (#41)

--- src/UUID.php
+++ src/UUID.php
@@ -59,6 +59,12 @@ class UUID
     public const TIME_OFFSET_INT = 0x01b21dd213814000;
 
     /** @internal */
+    private const INT_1E7 = 10_000_000;
+
+    /** @internal */
+    private const SUBSEC_BITS = 24;
+
+    /** @internal */
     private const UUID_REGEX = '/^(?:urn:)?(?:uuid:)?(\{)?([0-9a-f]{8})\-?([0-9a-f]{4})'
     . '\-?([0-9a-f]{4})\-?([0-9a-f]{4})\-?([0-9a-f]{12})(?(1)\}|)$/i';
 
@@ -138,13 +144,13 @@ class UUID
     /** @internal */
     private static function encodeSubsec(int $value): int
     {
-        return intdiv($value * 2 ** 24, 10 ** 7);
+        return intdiv($value << self::SUBSEC_BITS, self::INT_1E7);
     }
 
     /** @internal */
     private static function decodeSubsec(int $value): int
     {
-        return -(-$value * 10 ** 7 >> 24);
+        return -(-$value * self::INT_1E7 >> self::SUBSEC_BITS);
     }
 
     /**
@@ -198,7 +204,7 @@ class UUID
     public static function uuid6(): string
     {
         [$unixts, $subsec] = self::getUnixTime();
-        $timestamp = $unixts * 10 ** 7 + $subsec;
+        $timestamp = $unixts * self::INT_1E7 + $subsec;
         $timehex = str_pad(dechex($timestamp + self::TIME_OFFSET_INT), 15, '0', \STR_PAD_LEFT);
         $uhex = substr_replace(substr($timehex, -15), '6', -3, 0);
         $uhex .= bin2hex(random_bytes(8));
@@ -267,7 +273,8 @@ class UUID
         } elseif ($version === 7) {
             $unixts = hexdec(substr($timehex, 0, 10));
             $subsec = self::decodeSubsec(hexdec(substr($timehex, 10)));
-            $retval = substr_replace(str_pad(strval($unixts * 10 ** 7 + $subsec), 8, '0', \STR_PAD_LEFT), '.', -7, 0);
+            $retval = strval($unixts * self::INT_1E7 + $subsec);
+            $retval = substr_replace(str_pad($retval, 8, '0', \STR_PAD_LEFT), '.', -7, 0);
         }
         return $retval;
     }
